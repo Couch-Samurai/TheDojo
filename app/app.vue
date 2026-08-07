feaf851e5e0338eb4c2d98fef24d4a79e0ef3a51
JustinGreen <Justin.Green@stanchionpayments.com>
2026-08-07 09:30:31 +0200

    First Official Commit

---
+++ app/app.vue
@@ -0,0 +1,19 @@
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<template>
+  <div>
+    <NuxtLayout>
+        <img
+          src="~/assets/img/nuxt.jfif"
+          alt="Discover Nuxt"
+        >
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+  <!-- https://nuxt.com/docs/4.x/directory-structure/nuxt -->
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<!-- <template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template> -->
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+// Use a static import for server-side compatibility
+import '~/assets/css/main.css'
+
+// Caution: Dynamic imports are not server-side compatible
+// import('~/assets/css/first.css')
+</script>
